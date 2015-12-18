75a4baa42ed483fe9ca5b7b248069d4e9da3656a
Jason Housley <HousleyJK@gmail.com>
2015-12-17 17:11:44 -0700

    feat(util): Add rexports of mio utilities to facilitate timeouts

---
+++ src/util.rs
@@ -0,0 +1,8 @@
+//! The util module rexports some tools from mio in order to facilitate handling timeouts.
+
+/// Used to identify some timed-out event.
+pub use mio::Token;
+/// A handle to a specific timeout.
+pub use mio::Timeout;
+/// A Slab allocator for associating tokens to data.
+pub use mio::util::Slab;
