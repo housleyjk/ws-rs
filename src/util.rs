6cb57b280b400db00b532449ede57e261a8892b0
Hubert Figuière <hub@figuiere.net>
2016-02-24 15:42:25 -0500

    Remove slab from util.rs

--- src/util.rs
+++ src/util.rs
@@ -4,7 +4,3 @@
 pub use mio::Token;
 /// A handle to a specific timeout.
 pub use mio::Timeout;
-/// A Slab allocator for associating tokens to data.
-pub use slab;
-
-pub type Slab<T> = slab::Slab<T, Token>;
