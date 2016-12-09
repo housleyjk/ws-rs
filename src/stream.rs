b26e425f88393b9951f4a00016702c0c736cd3ba
Alexey Galakhov <agalakhov@gmail.com>
2016-12-09 15:14:11 +0100

    chore: Remove deprecated Mio 5 TryRed/TryWrite
    Signed-off-by: Alexey Galakhov <agalakhov@gmail.com>

--- src/stream.rs
+++ src/stream.rs
@@ -1,7 +1,7 @@
 use std::io;
+use std::io::ErrorKind::WouldBlock;
 use std::net::SocketAddr;
 
-use mio::deprecated::{TryRead, TryWrite};
 use mio::tcp::TcpStream;
 #[cfg(feature="ssl")]
 use openssl::ssl::SslStream;
@@ -11,7 +11,20 @@ use bytes::{Buf, MutBuf};
 
 use result::{Result, Error, Kind};
 
-pub trait TryReadBuf: TryRead {
+fn map_non_block<T>(res: io::Result<T>) -> io::Result<Option<T>> {
+    match res {
+        Ok(value) => Ok(Some(value)),
+        Err(err) => {
+            if let WouldBlock = err.kind() {
+                Ok(None)
+            } else {
+                Err(err)
+            }
+        }
+    }
+}
+
+pub trait TryReadBuf: io::Read {
     fn try_read_buf<B: MutBuf>(&mut self, buf: &mut B) -> io::Result<Option<usize>>
         where Self : Sized
     {
@@ -20,7 +33,7 @@ pub trait TryReadBuf: TryRead {
         // If your protocol is msg based (instead of continuous stream) you should
         // ensure that your buffer is large enough to hold an entire segment (1532 bytes if not jumbo
         // frames)
-        let res = self.try_read(unsafe { buf.mut_bytes() });
+        let res = map_non_block(self.read(unsafe { buf.mut_bytes() }));
 
         if let Ok(Some(cnt)) = res {
             unsafe { buf.advance(cnt); }
@@ -30,11 +43,11 @@ pub trait TryReadBuf: TryRead {
     }
 }
 
-pub trait TryWriteBuf: TryWrite {
+pub trait TryWriteBuf: io::Write {
     fn try_write_buf<B: Buf>(&mut self, buf: &mut B) -> io::Result<Option<usize>>
         where Self : Sized
     {
-        let res = self.try_write(buf.bytes());
+        let res = map_non_block(self.write(buf.bytes()));
 
         if let Ok(Some(cnt)) = res {
             buf.advance(cnt);
@@ -44,8 +57,8 @@ pub trait TryWriteBuf: TryWrite {
     }
 }
 
-impl<T: TryRead> TryReadBuf for T {}
-impl<T: TryWrite> TryWriteBuf for T {}
+impl<T: io::Read> TryReadBuf for T {}
+impl<T: io::Write> TryWriteBuf for T {}
 
 use self::Stream::*;
 pub enum Stream {
@@ -119,20 +132,20 @@ impl Stream {
     }
 }
 
-impl TryRead for Stream {
+impl io::Read for Stream {
 
-    fn try_read(&mut self, buf: &mut [u8]) -> io::Result<Option<usize>> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         match *self {
-            Tcp(ref mut sock) => sock.try_read(buf),
+            Tcp(ref mut sock) => sock.read(buf),
             #[cfg(feature="ssl")]
             Tls { ref mut sock, ref mut negotiating } => {
                 match sock.ssl_read(buf) {
-                    Ok(cnt) => Ok(Some(cnt)),
+                    Ok(cnt) => Ok(cnt),
                     Err(SslError::WantWrite(_)) => {
                         *negotiating = true;
-                        Ok(None)
+                        Err(io::Error::new(io::ErrorKind::WouldBlock, "SSL wants writing"))
                     },
-                    Err(SslError::WantRead(_)) => Ok(None),
+                    Err(SslError::WantRead(_)) => Err(io::Error::new(io::ErrorKind::WouldBlock, "SSL wants reading")),
                     Err(err) =>
                         Err(io::Error::new(io::ErrorKind::Other, err)),
                 }
@@ -141,27 +154,35 @@ impl TryRead for Stream {
     }
 }
 
-impl TryWrite for Stream {
+impl io::Write for Stream {
 
-    fn try_write(&mut self, buf: &[u8]) -> io::Result<Option<usize>> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         match *self {
-            Tcp(ref mut sock) => sock.try_write(buf),
+            Tcp(ref mut sock) => sock.write(buf),
             #[cfg(feature="ssl")]
             Tls { ref mut sock, ref mut negotiating } => {
 
                 *negotiating = false;
 
                 match sock.ssl_write(buf) {
-                    Ok(cnt) => Ok(Some(cnt)),
+                    Ok(cnt) => Ok(cnt),
                     Err(SslError::WantRead(_)) => {
                         *negotiating = true;
-                        Ok(None)
+                        Err(io::Error::new(io::ErrorKind::WouldBlock, "SSL wants reading"))
                     },
-                    Err(SslError::WantWrite(_)) => Ok(None),
+                    Err(SslError::WantWrite(_)) => Err(io::Error::new(io::ErrorKind::WouldBlock, "SSL wants writing")),
                     Err(err) =>
                         Err(io::Error::new(io::ErrorKind::Other, err)),
                 }
             }
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        match *self {
+            Tcp(ref mut sock) => sock.flush(),
+            #[cfg(feature="ssl")]
+            Tls { ref mut sock, negotiating: _ } => sock.flush(),
         }
     }
 }
