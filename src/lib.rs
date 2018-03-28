524d9c8a1993d796c908ec1ddfd7f02ea7ca4d6a
Corentin Henry <corentinhenry@gmail.com>
2018-03-27 17:33:04 -0700

    clippy: new_without_default_derive

--- src/lib.rs
+++ src/lib.rs
@@ -352,7 +352,7 @@ where
 }
 
 /// Utility for constructing a WebSocket from various settings.
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Default, Clone, Copy)]
 pub struct Builder {
     settings: Settings,
 }
@@ -361,9 +361,7 @@ pub struct Builder {
 impl Builder {
     /// Create a new Builder with default settings.
     pub fn new() -> Builder {
-        Builder {
-            settings: Settings::default(),
-        }
+        Builder::default()
     }
 
     /// Build a WebSocket using this builder and a factory.
